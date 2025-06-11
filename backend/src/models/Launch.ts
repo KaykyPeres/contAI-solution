0c39d8daa7d3e1b00f3b2ca3fbb4956b7529dd27
Kayky Plombon Peres <85962125+KaykyPeres@users.noreply.github.com>
2025-06-10 22:22:35 -0300

    feat: criar model Launch com campos principais

---
+++ backend/src/models/Launch.ts
@@ -0,0 +1,27 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+export enum LaunchType {
+  CREDITO = "Crédito",
+  DEBITO = "Débito",
+}
+
+@Entity()
+export class Launch {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  description: string;
+
+  @Column("decimal"{ precision: 10, scale: 2 })
+  amount: number;
+
+  @Column({
+    type: "enum", 
+    enum: LaunchType, 
+    default: LaunchType.DEBITO, 
+  })
+  type: LaunchType;
+
+  @Column()
+  date: Date;
+}
