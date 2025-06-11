1d7176b0d5f1dd60bd143b659d959ef16c1c4e42
Kayky Plombon Peres <85962125+KaykyPeres@users.noreply.github.com>
2025-06-11 00:10:10 -0300

    chore: configurar variaveis de ambiente

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -1,13 +1,14 @@
 import { DataSource } from "typeorm";
 import { Launch } from "./models/Launch";
+import 'dotenv/config';
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: "localhost",
-  port: 5432,
-  username: "usuario",
-  password: "senha",
-  database: "sistema_contabil",
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   synchronize: true,
   logging: true,
   entities: [Launch],
