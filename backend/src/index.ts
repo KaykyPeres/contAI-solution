a0e1f57706d2b8e0518f7efa97cf6428c95ab7b1
Kayky Plombon Peres <85962125+KaykyPeres@users.noreply.github.com>
2025-06-10 23:48:29 -0300

    feat: configurar servidor Express e aplicar rotas

---
+++ backend/src/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import cors from "cors";
+import { AppDataSource } from "./data-source";
+import launchRoutes from "./routes/launchRoutes";
+
+AppDataSource.initialize().then(() => {
+  const app = express();
+  app.use(cors());
+  app.use(express.json());
+  app.use("/launches", launchRoutes);
+  app.listen(3001, () => console.log("Servidor rodando na porta 3001"));
+});
