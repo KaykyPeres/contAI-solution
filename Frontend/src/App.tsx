d5be15453a252124724e2b9e88f6bff6576119f5
Kayky Plombon Peres <85962125+KaykyPeres@users.noreply.github.com>
2025-06-12 17:31:03 -0300

    feat: Exibe lista de lançamentos na tela inicial

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,35 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import axios from 'axios'; 
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface Launch {
+  id: number;
+  description: string;
+  amount: string;
+  type: 'Crédito' | 'Débito';
+  date: string; 
 }
 
-export default App
+function App() {
+  const [launches, setLaunches] = useState<Launch[]>([]);
+
+  useEffect(() => {
+    const fetchLaunches = async () => {
+      try {
+        const response = await axios.get('http://localhost:3001/launches');
+        setLaunches(response.data);
+      } catch (error) {
+        console.error("Erro ao buscar lançamentos:", error);
+      }
+    };
+
+    fetchLaunches(); 
+  }, []);
+
+  return (
+    <div>
+      <h1>Lançamentos Contábeis - ContAI</h1>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Descrição</th>
+            <th>Valor</th>
+            <th>Tipo</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+        <tbody>
+          {launches.map((launch) => (
+            <tr key={launch.id}>
+              <td>{launch.description}</td>
+              <td>R$ {launch.amount}</td>
+              <td className={launch.type === 'Crédito' ? 'credito' : 'debito'}>
+                {launch.type}
+              </td>
+              <td>{new Date(launch.date).toLocaleDateString('pt-BR', { timeZone: 'UTC' })}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default App;
