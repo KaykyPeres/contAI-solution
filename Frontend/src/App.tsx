abdcf5529c2a75eaf4bb9e27f5ac94960e0ce030
Kayky Plombon Peres <85962125+KaykyPeres@users.noreply.github.com>
2025-06-12 18:12:01 -0300

    fix: Formulario para adicionar novos lançamentos financeiros(app.tsx)

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,28 +1,28 @@
 import { useState, useEffect } from 'react';
-import axios from 'axios'; 
+import axios from 'axios';
 import './App.css';
+import { LaunchForm } from './components/LaunchForm'; 
 
 interface Launch {
   id: number;
   description: string;
   amount: string;
   type: 'Crédito' | 'Débito';
-  date: string; 
+  date: string;
 }
 
 function App() {
   const [launches, setLaunches] = useState<Launch[]>([]);
+  const fetchLaunches = async () => {
+    try {
+      const response = await axios.get('http://localhost:3001/launches');
+      setLaunches(response.data);
+    } catch (error) {
+      console.error("Erro ao buscar lançamentos:", error);
+    }
+  };
 
   useEffect(() => {
-    const fetchLaunches = async () => {
-      try {
-        const response = await axios.get('http://localhost:3001/launches');
-        setLaunches(response.data);
-      } catch (error) {
-        console.error("Erro ao buscar lançamentos:", error);
-      }
-    };
-
     fetchLaunches(); 
   }, []);
 
@@ -30,6 +30,11 @@ function App() {
     <div>
       <h1>Lançamentos Contábeis - ContAI</h1>
 
+      <LaunchForm onLaunchAdded={fetchLaunches} />
+
+      <hr />
+
+      <h2>Registros</h2>
       <table>
         <thead>
           <tr>
